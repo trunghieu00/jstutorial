63a3a2f1292a97f8ff66ce7a8ca973c0c74a7943
trunghieu00 <93066354+trunghieu00@users.noreply.github.com>
2021-11-14 09:30:42 +0700

    Update p5-search.js

--- usermanamgement/src/p5-search.js
+++ usermanamgement/src/p5-search.js
@@ -1,20 +1,15 @@
 const typeSelect = $("searchType");
 const searchInput = $("searchInput");
-
 const searchArr = [];
-
 const searchFunction = () => {
     const searchValue = searchInput.value;
     const searchedType = typeSelect.value;
-
     personsArr.forEach((e) => {
         if (e[searchedType] === searchValue) {
             searchArr.push(e);
         }
     });
-
-
-    $("myTable").remove();
+  $("myTable").remove();
     const newTable = creatTable(searchArr);
     form.append(newTable);
 };
